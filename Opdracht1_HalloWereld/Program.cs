e09d6d53ff1d2b47ce2d4ecaf989c243a666b6a1
tomad <tomad@LAPTOP-9Q32PNAB>
2022-09-01 20:04:38 +0200

    Add project files.

---
+++ Opdracht1_HalloWereld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht1_HalloWereld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
